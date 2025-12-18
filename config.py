28dc50abaa91179648143519455928d726faddeb
Sanjay-off <161325708+Sanjay-off@users.noreply.github.com>
2025-12-18 11:26:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,1 @@
-# Telegram Bot
-BOT_TOKEN = "8538894136:AAG0ZXlCHkNSmP6Yx-XEuJ72P1h5U5ihEwk"
-CHANNEL_ID = -1003537834995  # numeric channel ID
 
-MEGA_LINK = "https://mega.nz/folder/rl4lxChL#UX97Z3mY8qEXFPVVIzkBCQ"
-
-COURSE_NAME = "Education"
-# ZIP
-ZIP_PASSWORD = "Legalstuff123"
-ZIP_PART_SIZE_MB = 1900   # SAFE for bots
-ZIP_COMPRESSION_LEVEL = 1
-
-# Paths
-WORK_DIR = "work"
-DOWNLOAD_DIR = f"{WORK_DIR}/downloads"
-RENAMED_DIR = f"{WORK_DIR}/renamed"
-ZIP_DIR = f"{WORK_DIR}/zips"
-
-QUOTA_SLEEP_SECONDS = 3600 
